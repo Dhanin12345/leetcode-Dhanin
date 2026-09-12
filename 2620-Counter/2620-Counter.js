0c06c53802f10898f06ca25fd291e55d61591504
DHANIN T <dhanin.t2007@gmail.com>
2026-09-12 10:20:04 +0530

    Create 2620-Counter

---
+++ 2620-Counter/2620-Counter.js
@@ -0,0 +1,13 @@
+// Last updated: 9/12/2026, 10:20:03 AM
+var createCounter = function(n) {
+    return function() {
+        return n++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
