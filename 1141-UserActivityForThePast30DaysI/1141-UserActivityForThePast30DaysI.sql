84627bdf7f7f8c66091314345af2b5c66d854676
DHANIN T <dhanin.t2007@gmail.com>
2026-09-12 10:22:47 +0530

    Create 1141-UserActivityForThePast30DaysI

---
+++ 1141-UserActivityForThePast30DaysI/1141-UserActivityForThePast30DaysI.sql
@@ -0,0 +1,7 @@
+-- Last updated: 9/12/2026, 10:22:47 AM
+SELECT
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+GROUP BY activity_date;
