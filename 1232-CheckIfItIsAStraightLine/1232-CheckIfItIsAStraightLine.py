282576d64cba0b6f96fe29aefe0b0c3a2ccb9ea4
DHANIN T <dhanin.t2007@gmail.com>
2026-09-12 10:22:28 +0530

    Create 1232-CheckIfItIsAStraightLine

---
+++ 1232-CheckIfItIsAStraightLine/1232-CheckIfItIsAStraightLine.py
@@ -0,0 +1,11 @@
+# Last updated: 9/12/2026, 10:22:27 AM
+class Solution:
+    def checkStraightLine(self, coordinates):
+        x0, y0 = coordinates[0]
+        x1, y1 = coordinates[1]
+
+        for x, y in coordinates[2:]:
+            if (y1 - y0) * (x - x0) != (y - y0) * (x1 - x0):
+                return False
+
+        return True
