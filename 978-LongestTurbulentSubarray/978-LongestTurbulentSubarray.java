7c07d92a9a666ec22a03789605fc372089525ed0
DHANIN T <dhanin.t2007@gmail.com>
2026-09-12 10:23:38 +0530

    Create 978-LongestTurbulentSubarray

---
+++ 978-LongestTurbulentSubarray/978-LongestTurbulentSubarray.java
@@ -0,0 +1,35 @@
+// Last updated: 9/12/2026, 10:23:37 AM
+class Solution {
+    public int maxTurbulenceSize(int[] arr) {
+
+        int n = arr.length;
+
+        if (n == 1) {
+            return 1;
+        }
+
+        int up = 1;
+        int down = 1;
+        int answer = 1;
+
+        for (int i = 1; i < n; i++) {
+
+            if (arr[i] > arr[i - 1]) {
+                up = down + 1;
+                down = 1;
+            }
+            else if (arr[i] < arr[i - 1]) {
+                down = up + 1;
+                up = 1;
+            }
+            else {
+                up = 1;
+                down = 1;
+            }
+
+            answer = Math.max(answer, Math.max(up, down));
+        }
+
+        return answer;
+    }
+}
