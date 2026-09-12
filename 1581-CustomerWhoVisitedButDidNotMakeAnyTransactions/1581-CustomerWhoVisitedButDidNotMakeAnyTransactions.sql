405ce800e4de1fe573106fc7f0dbccbcfccfa1c9
DHANIN T <dhanin.t2007@gmail.com>
2026-09-12 10:21:21 +0530

    Create 1581-CustomerWhoVisitedButDidNotMakeAnyTransactions

---
+++ 1581-CustomerWhoVisitedButDidNotMakeAnyTransactions/1581-CustomerWhoVisitedButDidNotMakeAnyTransactions.sql
@@ -0,0 +1,9 @@
+-- Last updated: 9/12/2026, 10:21:21 AM
+SELECT
+    v.customer_id,
+    COUNT(*) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t
+ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id;
