25733db9f7132969b5d47426936b3c5a5c088213
DHANIN T <dhanin.t2007@gmail.com>
2026-09-12 10:23:52 +0530

    Create 933-NumberOfRecentCalls

---
+++ 933-NumberOfRecentCalls/933-NumberOfRecentCalls.py
@@ -0,0 +1,19 @@
+# Last updated: 9/12/2026, 10:23:50 AM
+from collections import deque
+
+class RecentCounter:
+
+    def __init__(self):
+        self.q = deque()
+
+    def ping(self, t):
+        self.q.append(t)
+
+        while self.q[0] < t - 3000:
+            self.q.popleft()
+
+        return len(self.q)
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
