60e6e952a564872f885708d43874aec388f19b89
DHANIN T <dhanin.t2007@gmail.com>
2026-09-12 10:23:06 +0530

    Create 1070-ProductSalesAnalysisIii

---
+++ 1070-ProductSalesAnalysisIii/1070-ProductSalesAnalysisIii.sql
@@ -0,0 +1,16 @@
+-- Last updated: 9/12/2026, 10:23:05 AM
+SELECT
+    s.product_id,
+    s.year AS first_year,
+    s.quantity,
+    s.price
+FROM Sales s
+JOIN (
+    SELECT
+        product_id,
+        MIN(year) AS first_year
+    FROM Sales
+    GROUP BY product_id
+) f
+ON s.product_id = f.product_id
+AND s.year = f.first_year;
